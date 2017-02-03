e13deb232753da384d99c9ecac09665d2d48b74d
Midenko <kostyagrand@mail.ru>
2017-02-03 19:23:58 +0400

    Add files via upload

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    	this.high = 0;
+    	this.low = 0;
+    	this.mid = 0;
+    }
 
     setRange(min, max) {
-
+    	this.high = max;
+    	this.low = min;
+    	this.mid = this.low + (this.low + this.high) / 2;
     }
 
     guess() {
-
+    	this.mid = Math.round( (this.low + this.high) / 2 );
+    	return this.mid;
     }
 
     lower() {
-
+    	this.high = this.mid - 1;
     }
 
     greater() {
-
+    	this.low = this.mid + 1;
     }
 }
 
